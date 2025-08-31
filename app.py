cfa1ea0ce8836a349b2741683758bc2cf1ef98bf
Harsh Gupta <guptaharsh311@gmail.com>
2025-09-01 00:33:07 +0530

    Add feature to generate random order data

--- app.py
+++ app.py
@@ -23,15 +23,23 @@ class Customer():
         self.email=email
 
     def addCustomerData():
+        customer=[]
         for i in range (50):
-            query='INSERT INTO CUSTOMER(first_name, last_name, phone_number, email) values (%s, %s, %s, %s)'
+            query='INSERT INTO customer(first_name, last_name, phone_number, email) values (%s, %s, %s, %s)'
             firstName=faker.first_name()
             lastName=faker.last_name()
             phoneNumber=faker.phone_number()
             email=faker.email()
-            value=(firstName,lastName,phoneNumber,email)
-            cursor.executemany(query,value)
+            customer.append((firstName,lastName,phoneNumber,email))
+        cursor.executemany(query,customer)
         connection.commit()
+
+    def getCustomerData():
+        query='SELECT * FROM customer'
+        cursor.execute(query)
+        result=cursor.fetchall()
+        result=[x[0] for x in result]
+        return result
 
 class Product():
     def __init__(self, productName, displayName, category):
@@ -45,18 +53,62 @@ class Product():
         return f"{random.choice(adjectives)} {random.choice(product_types)}"
     
     def addProductData():
+        product=[]
         for i in range(50):
             productName=Product.create_product_name()
             displayName=productName
             category=random.choice(['Electronics', 'Fashion', 'Home & Kitchen', 'Sports', 'Books'])
-            query='INSERT INTO PRODUCT(product_name, display_name, category) VALUES (%s, %s, %s)'
-            value=(productName, displayName, category)
-            cursor.executemany(query, value)
+            query='INSERT INTO product(product_name, display_name, category) VALUES (%s, %s, %s)'
+            product.append((productName, displayName, category))
+        cursor.executemany(query, product)
         connection.commit()
-    
+
+    def getProductData():
+        query='SELECT * FROM product'
+        cursor.execute(query)
+        result=cursor.fetchall()
+        result=[x[0] for x in result]
+        return result
+
+class Orders():
+    def __init__(self, order_id, customer_id, product_id, quantity, order_date):
+        self.order_id = order_id
+        self.customer_id = customer_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.order_date = order_date
+
+    def addOrderData():
+        orders = []
+        for i in range(50):
+            customer_id = random.choice(Customer.getCustomerData())
+            order_amount = random.uniform(25, 10000)
+            order_date = faker.date_between(start_date='-1y', end_date='today')
+            orders.append((customer_id, order_amount, order_date))
+        query = 'INSERT INTO order_details(customer_id, order_amount, order_date) VALUES (%s, %s, %s)'
+        cursor.executemany(query, orders)
+        connection.commit()
+
+    def getOrderData():
+        query = 'SELECT * FROM order_details'
+        cursor.execute(query)
+        result = cursor.fetchall()
+        result = [x[0] for x in result]
+        return result
+
+    def addProductOrderMapping():
+        product_order_mapping = []
+        for i in range(50):
+            order_id = random.choice(Orders.getOrderData())
+            quantity = random.uniform(1, 5)
+            product_id=random.choice(Product.getProductData())
+            product_order_mapping.append((order_id, product_id, quantity))
+        query = 'INSERT INTO product_order_mapping(order_id, product_id, quantity) VALUES (%s, %s, %s)'
+        cursor.executemany(query, product_order_mapping)
+        connection.commit()
 
 
-#eEstablish connection to mysql server on local machine/localhost
+#Establish connection to mysql server on local machine/localhost
 connection = mysql.connector.connect(
     host='localhost',
     user='harsh',
@@ -71,16 +123,20 @@ print('Welcome to the Customer Analysis mini-project !')
 while userChoice !=4:
     print('Press 1: To create random data of 50 customers')
     print('Press 2: To create random data of 50 products')
-    print('Press 3: To show dashboard')
-    print('Press 4: To exit')
+    print('Press 3: To create random data of 50 orders')
+    print('Press 4: To show dashboard')
+    print('Press 5: To exit')
     userChoice = int(input())
     if userChoice==1:
         Customer.addCustomerData()
     elif userChoice==2:
         Product.addProductData()
     elif userChoice==3:
-        pass
+        Orders.addOrderData()
+        Orders.addProductOrderMapping()
     elif userChoice==4:
+        pass
+    elif userChoice==5:
         print('Thanks for trying the project !!')
     else:
         print('Please try a valid input')
